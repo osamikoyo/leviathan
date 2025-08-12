4d4f84e4645064c337283d7dae8c74dbf19ddab9
osamikoyo <v.pylayev@mail.ru>
2025-08-12 07:45:21 +0500

    add reader client

---
+++ reader/reader_client.go
@@ -0,0 +1,33 @@
+package reader
+
+import (
+	"errors"
+
+	"github.com/osamikoyo/leviathan/models"
+)
+
+type ReaderClient struct {
+	output chan *models.ReadRequest
+}
+
+func NewReaderClient(output chan *models.ReadRequest) *ReaderClient {
+	return &ReaderClient{
+		output: output,
+	}
+}
+
+func (r *ReaderClient) Read(sql string, args ...any) (Result, error) {
+	if len(sql) == 0 {
+		return nil, errors.New("nil input")
+	}
+
+	resp := make(chan *models.ReadResponse, 1)
+
+	req := models.NewReadRequest(sql, args, resp)
+
+	r.output <- req
+
+	result := <-resp
+
+	return result.Values, result.Error
+}
