6c5e030d7b6674252befab19496c485b22bbc56a
osamikoyo <v.pylayev@mail.ru>
2025-08-18 07:46:37 +0500

    update models request

--- models/request.go
+++ models/request.go
@@ -11,10 +11,11 @@ type Request struct {
 	Timestamp    time.Time     `json:"timestamp"`
 }
 
-func NewRequest(sql string, writerequest bool) *Request {
+func NewRequest(sql string, args []interface{}, writerequest bool) *Request {
 	return &Request{
 		Timestamp:    time.Now(),
 		SQL:          sql,
+		Args:         args,
 		WriteRequest: writerequest,
 	}
 }
